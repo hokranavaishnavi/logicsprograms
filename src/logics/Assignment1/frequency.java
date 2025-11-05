289d9ce4b43efab4f9eb9160103d7f4b248743b8
123Vaishu <vaishnavi@clistas.com>
2025-11-05 17:11:07 +0530

    file modified

---
+++ src/logics/Assignment1/frequency.java
@@ -0,0 +1,35 @@
+package logics.Assignment1;
+
+import java.util.Scanner;
+
+public class frequency {
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+
+            System.out.print("Enter size: ");
+            int n = sc.nextInt();
+            int[] a = new int[n];
+            int[] b = new int[n];
+            int[] c = new int[n];
+
+            System.out.println("Enter " + n + " numbers:");
+            for (int i = 0; i < n; i++) {
+                a[i] = sc.nextInt();
+            }
+
+            for (int i = 0; i < n; i++) {
+                int count = 1;
+                if(a[i] == -1) continue;
+                for (int j = i + 1; j < n; j++) {
+                    if (a[i] == a[j]) {
+                        count++;
+                        a[j] = -1;
+                    }
+
+                }
+                System.out.println(a[i] + " = " + count);
+            }
+
+        }
+    }
+
--- src/logics/Assignment1/leftrotation.java
+++ src/logics/Assignment1/leftrotation.java
@@ -1,20 +1,21 @@
-package logics;
+package logics.Assignment1;
 
 import java.util.Arrays;
 
-public class rightrotation {
+public class leftrotation {
+
         public static void main(String[] args) {
             int[] arr={1,2,3,4,5,6};
             int r=2;
             System.out.println(Arrays.toString(solution(arr,r)));
         }
-        static int[] solution(int[]arr,int r){
+        static int[] solution(int[] arr,int r){
             for (int i = 0; i < r; i++) {
-                int last= arr[arr.length-1];
-                for (int j = arr.length-1; j >0 ; j--) {
-                    arr[j]=arr[j-1];
+                int first=arr[0];
+                for (int j = 0; j < arr.length-1; j++) {
+                    arr[j]=arr[j+1];
                 }
-                arr[0]= last;
+                arr[arr.length-1]=first;
             }
             return arr;
         }
