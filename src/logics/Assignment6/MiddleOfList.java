f7f2bce5413449b2c97183a43b17a35674b23577
123Vaishu <vaishnavi@clistas.com>
2025-11-16 19:03:55 +0530

    project added updated2

---
+++ src/logics/Assignment6/MiddleOfList.java
@@ -0,0 +1,31 @@
+package logics.Assignment6;
+
+public class MiddleOfList {
+    static class Node {
+        int data;
+        Node next;
+        Node(int d) { data = d; next = null; }
+    }
+
+    static Node findMiddle(Node head) {
+        if (head == null) return null;
+        Node slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+    public static void main(String[] args) {
+        Node head = new Node(10);
+        head.next = new Node(20);
+        head.next.next = new Node(30);
+        head.next.next.next = new Node(40);
+        head.next.next.next.next = new Node(50);
+
+        Node middle = findMiddle(head);
+        System.out.println("Middle element is: " + (middle != null ? middle.data : "None"));
+    }
+}
+
